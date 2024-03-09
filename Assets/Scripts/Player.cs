09a8b00fd7fec0bdd44d12321d9be1979eba5ec2
rystills <rystills@gmail.com>
2024-03-09 17:05:17 -0500

    add radiation overheat mechanic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
 
+public enum DeathBy
+{
+    Radiation,
+    RadiationOverheat,
+}
+
 public class Player : MonoBehaviour
 {
     public static new Transform transform;
     public static Player instance;
 
-    public static void Die()
+    public static void Die(DeathBy method)
     {
-        RadiationManager.radiationLevel = 0;
+        RadiationManager.heatLevel = RadiationManager.radiationLevel = 0;
     }
 
     private void Awake()
--- Assets/Scripts/RadiationManager.cs
+++ Assets/Scripts/RadiationManager.cs
@@ -6,14 +6,32 @@ public class RadiationManager : MonoBehaviour
     [SerializeField] private HingeDoor door;
     [SerializeField] private float radiationIncr;
     [SerializeField] private float radiationDecr;
+    [SerializeField] private float heatIncr;
+    [SerializeField] private float heatDecr;
+    [SerializeField] private Light metaballLight;
+    [SerializeField] private Color coolColor;
+    [SerializeField] private Color hotColor;
+    [SerializeField] private float coolStrength;
+    [SerializeField] private float hotStrength;
     public static float radiationLevel;
+    public static float heatLevel;
 
     private void Update()
     {
-        radiationLevel = Mathf.Clamp01(radiationLevel + (door.openPercentage * radiationIncr
-                                                      - (int)(1 - door.openPercentage + .01f) * radiationDecr) * Time.deltaTime);
+        // increase radiation while door is open
+        float radDelta = (door.openPercentage * radiationIncr - (int)(1 - door.openPercentage + .01f) * radiationDecr) * Time.deltaTime;
+        radiationLevel = Mathf.Clamp01(radiationLevel + radDelta);
+        if (radiationLevel == 1) Player.Die(DeathBy.Radiation);
+
+        // increase heat while door is closed
+        float heatDelta = (door.openPercentage * -heatDecr + (int)(1 - door.openPercentage + .01f) * heatIncr) * Time.deltaTime;
+        heatLevel = Mathf.Clamp01(heatLevel + heatDelta);
+        if (heatLevel == 1) Player.Die(DeathBy.RadiationOverheat);
+        
+        // apply effects
         mat.SetFloat("strength", radiationLevel);
         
-        if (radiationLevel == 1) Player.Die();
+        metaballLight.intensity = Mathf.Lerp(coolStrength, hotStrength, heatLevel);
+        metaballLight.color = Color.Lerp(coolColor, hotColor, heatLevel);
     }
 }
