a9410b897d56a49555eeda0f0955a09850e7b2b6
rystills <rystills@gmail.com>
2024-04-14 06:53:39 -0400

    cycle RGB LEDs on left/right click, cycle dominant LEDs over time

---
+++ Assets/Scripts/LEDLight.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LEDLight : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField] private new Light light;
+    [SerializeField] private MeshRenderer lightMR;
+    private Material lightMat;
+    private Color[] lightColors = { Color.red,
+                                    Color.green,
+                                    Color.blue };
+    private Color[] lightMatEmissionColors = { new(2f, 0f, 0f, 4f),
+                                               new(0f, 2f, 0f, 4f),
+                                               new(0f, 0f, 2f, 4f) };
+    public int colInd;
+
+    private void Awake()
+    {
+        lightMat = lightMR.material;
+        lightMat.EnableKeyword("_EMISSION");
+        lightMat.SetColor("_EmissionColor", lightColors[0]);
+    }
+
+    public void SetColInd(int newInd)
+    {
+        light.color = lightColors[colInd = newInd];
+        lightMat.SetColor("_EmissionColor", lightMatEmissionColors[colInd]);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+        => SetColInd((colInd + (eventData.button == PointerEventData.InputButton.Left ? 1 : lightColors.Length - 1)) % lightColors.Length);
+}
--- Assets/Scripts/LEDMachine.cs
+++ Assets/Scripts/LEDMachine.cs
@@ -5,10 +5,12 @@ public class LEDMachine : MonoBehaviour
 {
     public static LEDMachine instance;
 
-    [SerializeField] private List<Light> lights;
+    [SerializeField] private List<LEDLight> lights;
     private float syncedAtTime;
     public static float desyncAmount;
     [SerializeField] private float syncDuration;
+    private int[] rgbNum = new int[3];
+    private int rgbMax;
 
     private void Awake()
     {
@@ -29,10 +31,23 @@ public class LEDMachine : MonoBehaviour
 
     private void LateUpdate()
     {
+        // determine the most common color
+        rgbNum[0] = rgbNum[1] = rgbNum[2] = 0;
+        for (int i = 0; i < lights.Count; ++i) ++rgbNum[lights[i].colInd];
+        rgbMax = rgbNum[0] > rgbNum[1] ? 0 : 1;
+        rgbMax = rgbNum[rgbMax] > rgbNum[2] ? rgbMax : 2;
+
         // desync after syncDuration elapses
-        if (syncedAtTime >= 0 && TimeExt.Since(syncedAtTime) > syncDuration / GameState.waveFactor)
+        if (syncedAtTime >= 0 && TimeExt.Since(syncedAtTime) > syncDuration / GameState.ledFactor)
         {
-            syncedAtTime = -1;
+            syncedAtTime += syncDuration;
+
+            // select a random light of the most common color
+            int swapInd;
+            do { swapInd = Random.Range(0, lights.Count); } while (lights[swapInd].colInd != rgbMax);
+
+            // swap the color
+            lights[swapInd].SetColInd((lights[swapInd].colInd + Random.Range(1, 3)) % 3);
         }
 
         // rotate over time
